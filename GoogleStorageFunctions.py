1b357d647ccec5d0f84aaafcce1e5358caca8924
Mamonu <thorosm@msn.com>
2018-08-28 02:39:42 +0100

    Create GoogleStorageFunctions.py

---
+++ GoogleStorageFunctions.py
@@ -0,0 +1,26 @@
+
+
+def create_bucket(bucket_name):
+    """Creates a new bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.create_bucket(bucket_name)
+    print('Bucket {} created'.format(bucket.name))
+
+def delete_bucket(bucket_name):
+    """Deletes a bucket. The bucket must be empty."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    bucket.delete()
+    print('Bucket {} deleted'.format(bucket.name))
+
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
+
+    print('File {} uploaded to {}.'.format(
+        source_file_name,
+        destination_blob_name))
